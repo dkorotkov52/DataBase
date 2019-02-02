bf781727461d2e9fd32e4b15662cde874e3fb1a9
korotkov52 <dkorotkov52@yandex.ru>
2019-02-02 16:38:19 +0300

    проект sqlite +

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,46 +2,37 @@ package com.example.sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import butterknife.BindView;
-import butterknife.OnClick;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.name)
-    EditText name;
-    @BindView(R.id.email)
-    EditText email;
-    @BindView(R.id.add)
-    Button add;
-    @BindView(R.id.read)
-    Button read;
-    @BindView(R.id.remove)
-    Button remove;
+    private EditText name;
+    private EditText email;
+    private Button add;
+    private Button read;
+    private Button remove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
     }
 
-    @OnClick({R.id.add, R.id.read, R.id.remove})
-    public void onClick(View view) {
-        String name = this.name.getText().toString();
-        String email = this.email.getText().toString();
-
-        switch (view.getId()) {
+    @Override
+    public void onClick(View v) {
+        String name = String.valueOf(this.name.getText());
+        String email = String.valueOf(this.email.getText());
+        switch (v.getId()) {
             case R.id.add:
-                //TODO
                 break;
             case R.id.read:
-                //TODO
                 break;
             case R.id.remove:
-                //TODO
                 break;
         }
     }
