b93438d684a9e63e11604a33bd1d6459673d0516
korotkov52 <dkorotkov52@yandex.ru>
2019-02-03 10:47:05 +0300

    try

--- app/src/main/java/com/example/sqlite/MainPresenter.java
+++ app/src/main/java/com/example/sqlite/MainPresenter.java
@@ -1,59 +1,71 @@
 package com.example.sqlite;
 
 import android.app.Presentation;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.util.List;
 import java.util.Observable;
 
-class MainPresenter{
+class MainPresenter {
     private MainView view;
 
     public MainPresenter(MainView view) {
         this.view = view;
     }
 
-    void getPeooples (){
+    void getPeooples() {
         List<People> peoples;
         peoples = App.getNewInstanse().getDatabase().peopleDao().getAll();
         view.showList(peoples);
     }
 
-    void addPeople(String name, String email){
+    void addPeople(String name, String email) {
         Log.e("MyLog", "name " + name + " email " + email);
         People people = new People();
         people.name = name;
         people.email = email;
         try {
             App.getNewInstanse().getDatabase().peopleDao().add(people);
-        }
-        catch (Exception e){
-
+        } catch (Exception e) {
+            view.showToast("Не удалось");
         }
         view.showToast("Добавлено");
     }
 
-    void removePeople(String name, String email){
+    void removePeople(String name, String email) {
         People people = new People();
         people.name = name;
         people.email = email;
         Log.e("MyLog", "remove name " + name);
-
-        App.getNewInstanse().getDatabase().peopleDao().remove(people);
+        try {
+            App.getNewInstanse().getDatabase().peopleDao().remove(people);
+        } catch (Exception e) {
+            view.showToast("Не удалось");
+        }
         view.showToast("Удалено");
     }
 
-    void getPeople(String name){
+    void getPeople(String name) {
         Log.e("MyLog", "name " + name);
-        People people = App.getNewInstanse().getDatabase().peopleDao().getById(name);
-        view.showToast("Имя: " + people.name + " email " + people.email);
+        try {
+            People people = App.getNewInstanse().getDatabase().peopleDao().getById(name);
+            view.showToast("Имя: " + people.name + " email " + people.email);
+        } catch (Exception e) {
+            view.showToast("Не удалось");
+        }
     }
 
     public void allDelete() {
         List<People> peoples;
         peoples = App.getNewInstanse().getDatabase().peopleDao().getAll();
-        for (int i = 0; i < peoples.size(); i++){
+        for (int i = 0; i < peoples.size(); i++) {
+            try {
             App.getNewInstanse().getDatabase().peopleDao().remove(peoples.get(i));
+            }
+            catch (Exception e){
+                view.showToast("Не удалось");
+            }
         }
         view.showToast("Почистил БД");
     }
